83d0d387adddca80cab63c5db281ad90c4f5f0bc
DHLarsen <Daniel.holm.larsen@gmail.com>
2023-10-21 16:36:50 +0200

    Started work on MoveController

--- Assets/Scripts/MoveController.cs
+++ Assets/Scripts/MoveController.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class MoveController : MonoBehaviour
 {
+    public Rigidbody2D rigidbody2D;
+    public float moveDistance;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,25 @@ public class MoveController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown("up"))
+        {
+            rigidbody2D.MovePosition(rigidbody2D.position + Vector2.up * moveDistance);
+        }
+        else if (Input.GetKeyDown("down"))
+        {
+            rigidbody2D.MovePosition(rigidbody2D.position + Vector2.down * moveDistance);
+        }
+        else if (Input.GetKeyDown("left"))
+        {
+            rigidbody2D.MovePosition(rigidbody2D.position + Vector2.left * moveDistance);
+        }
+        else if (Input.GetKeyDown("right"))
+        {
+            rigidbody2D.MovePosition(rigidbody2D.position + Vector2.right * moveDistance);
+        }
         
+
+        
+
     }
 }
